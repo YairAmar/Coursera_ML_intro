ae08fe93bba3029e30b1209757f83aecb4aef764
Yair Amar <yairamr@gmail.com>
2021-05-23 13:36:54 +0300

    Stam

--- ex1/Utils.py
+++ ex1/Utils.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
+from typing import Dict, Callable
 
 
-def compute_cost(x, y, theta):
-    """ Computes the cost function's value
+def compute_cost(x: np.array, y: np.array, theta: np.array) -> np.float:
+    """
+    Computes the cost function's value
 
-    Keyword arguments:
-    x -- input data, with a column of 1
-    y -- input target
-    theta -- weights of the linear regression
+    Args:
+        x: input data, with a column of 1
+        y: input target
+        theta: weights of the linear regression
+
+    Returns:
+        cost: the value of the cost function
     """
     m = len(y)
     y_hat = x @ theta
@@ -17,25 +22,26 @@ def compute_cost(x, y, theta):
     return cost
 
 
-def feature_normalize(x):
-    """ Normalizes data to be ~ N(0,1)
+def feature_normalize(x: np.array):
+    """
+    Normalizes data to be ~ N(0,1)
 
-    Keyword arguments:
-    x -- input data
+    Args:
+        x: input data
     """
     def normalize(vec): return (vec - np.mean(vec)) / np.std(vec)
     for i in np.arange(x.shape[1] - 1) + 1:
         x[:, i] = normalize(x[:, i])
 
 
-def plot_model(x, y, theta):
+def plot_model(x: np.array, y: np.array, theta: np.array):
     """ Visualizes a trained regression model, while plotting the data as well
     Works only for 1-d data
 
-    Keyword arguments:
-    x -- the input data
-    y -- target
-    theta -- weights of the linear regression
+    Args:
+        x: the input data
+        y: target
+        theta: weights of the linear regression
     """
     plt.scatter(x[:, -1], y, marker="3")
     plt.xlabel("x1")
@@ -48,15 +54,16 @@ def plot_model(x, y, theta):
     plt.show()
 
 
-def visualize_optimization(x, y, doc_dict, f):
-    """ Creates a 3-d surface plot for the given cost-function, with the values that theta got through the training
+def visualize_optimization(x: np.array, y: np.array, doc_dict: Dict, f: Callable):
+    """
+    Creates a 3-d surface plot for the given cost-function, with the values that theta got through the training
     Works only for 1-d data
 
-    Keyword arguments:
-    x -- the input data
-    y -- target
-    doc_dict -- output of the fit function in LinearRegression class
-    f -- function that computes the wanted cost function
+    Args:
+        x: input data
+        y: target
+        doc_dict: output of the fit function in LinearRegression class
+        f: function that computes the wanted cost function
     """
     fig = plt.figure(figsize=(12, 12))
     ax = fig.gca(projection='3d')
@@ -85,12 +92,13 @@ def visualize_optimization(x, y, doc_dict, f):
     plt.show()
 
 
-def plot_cost(doc):
-    """ Plots the cost function over training iterations
+def plot_cost(doc: Dict):
+    """
+    Plots the cost function over training iterations
 
-    Keyword arguments:
-    doc -- output of the fit function in LinearRegression class
-          a dictionary with the key "cost", and it's fitting values.
+    Args:
+        doc: output of the fit function in LinearRegression class
+        a dictionary with the key "cost", and it's fitting values.
     """
     plt.plot(doc["cost"])
     plt.xlabel("iterations (dozens)")
@@ -100,12 +108,18 @@ def plot_cost(doc):
     plt.show()
 
 
-def load_data(path):
-    """ Loading data and formatting for the latter linear regression
-
-    Keyword arguments:
-    path -- directory path of the csv file containing the data
+def load_data(path: str) -> tuple:
     """
+    Loading data and formatting for the latter linear regression
+
+    Args:
+        path: directory path of the csv file containing the data
+
+    Returns:
+        x: an array with the data
+        y: target values
+    """
+    print(path)
     data1 = np.loadtxt(path, delimiter=',', unpack=True)
     x = np.array(data1[:-1]).T
     y = np.array(data1[-1:]).T
