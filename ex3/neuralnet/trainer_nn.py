d88a21715fb95fe368908bfe1cebae296492285f
Yair Amar <yairamr@gmail.com>
2021-06-03 18:05:00 +0300

    ready for CR for real.

--- ex3/neuralnet/trainer_nn.py
+++ ex3/neuralnet/trainer_nn.py
@@ -2,22 +2,21 @@ import numpy as np
 from utils import sigmoid
 
 
-def forward(theta1: np.ndarray, theta2: np.ndarray, x: np.ndarray) -> np.ndarray:
+def forward(weights: list, x: np.ndarray) -> np.ndarray:
     """
     Applies feed-forward on the given neural network
 
     Args:
-        theta1: weights of the 1st layer in the neural-net
-        theta2: weights of the 2nd layer in the neural-net
+        weights: weights of the neural-net, layer by layer
         x: input data
 
     Returns:
         probability of each class
     """
-    a1 = np.insert(x.T, 0, 1, axis=0)
-    z2 = theta1 @ a1
-    a2 = sigmoid(z2)
-    a2 = np.insert(a2, 0, 1, axis=0)
-    z3 = theta2 @ a2
-    a3 = sigmoid(z3)
-    return a3
+    a = x.T
+    for layer in weights:
+        a = np.insert(a, 0, 1, axis=0)
+        z = layer @ a
+        a = sigmoid(z)
+
+    return a
